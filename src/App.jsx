90360749f3fe97c8b135723ed4abf961aafed69b
KhanFahad1448 <fk9719650@gmail.com>
2025-10-15 01:59:19 +0530

    implemented scroll-to-top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
 import Products from "./Pages/Products";
 import Cart from "./Pages/Cart";
-import Checkout from "./Pages/Checkout"; // NEW
+import Checkout from "./Pages/Checkout";
 import Login from "./Pages/Login";
 import AdminDashboard from "./Pages/AdminDashboard";
 import AdminOrders from "./Pages/AdminOrders";
@@ -22,6 +22,21 @@ import { CartProvider } from "./Context/CartContext";
 import { LabTestsProvider } from "./Context/LabTestsContext";
 import { Toaster } from "react-hot-toast";
 
+// Import ScrollToTop component
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+// ScrollToTop component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+}
+
 function AppRoutes() {
   const { user } = useAuth();
   const isAdmin = user?.role === "admin";
@@ -32,7 +47,7 @@ function AppRoutes() {
       <Route path="/home" element={<Home />} />
       <Route path="/products" element={<Products />} />
       <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} /> {/* NEW ROUTE */}
+      <Route path="/checkout" element={<Checkout />} />
       <Route path="/login" element={<Login />} />
 
       {/* Admin Routes */}
@@ -59,6 +74,7 @@ function App() {
         <LabTestsProvider>
           <Router>
             <Toaster position="top-right" reverseOrder={false} />
+            <ScrollToTop /> {/* Add ScrollToTop here */}
             <div className="flex flex-col min-h-screen">
               <div className="fixed top-0 left-0 w-full z-50">
                 <Navbar />
